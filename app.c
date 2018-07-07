393fecb39542441fb546b65e94d3a0e2c343893b
Marcos Pivetta <marcospivetta60@gmail.com>
2018-07-07 17:46:10 -0300

    envio de pacote pelo servidor

--- app.c
+++ app.c
@@ -21,13 +21,16 @@ struct ip_list_struct {
     char **ips; 
 };
 
-struct loc_info_struct {
-	int socket_desc, 
-		client_sock,
-		c, 
-		*new_sock,
-		port;
+struct server_info_struct {
+	struct sockaddr_in loc_addr;
+	int server_socket_desc;
 };
+
+typedef struct {
+	uint8_t size;
+	char method;
+	char payload[5];
+} aatp_msg;
 
 void initialize_ip_list_struct(struct ip_list_struct *ip_list){
 	ip_list->num_ips = 0;
@@ -69,141 +72,154 @@ void build_ip_array(struct ip_list_struct *ip_list){
 	// ===================================================================
 }
 
-void *connection_handler(void *socket_desc){
-    //Get the socket descriptor
-    int sock = *(int*)socket_desc;
-    int read_size;
-    char *message , buffer[6];
+void initialize_server_info(int port, struct server_info_struct *server_info){
+	server_info->loc_addr.sin_family = AF_INET;
+	server_info->loc_addr.sin_addr.s_addr = INADDR_ANY;
+	server_info->loc_addr.sin_port = htons(port);
 
-    while( (read_size = recv(sock , buffer , sizeof(buffer) , 0)) > 0 )
+	server_info->server_socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+};
+
+int initialize_server_socket(struct server_info_struct *server_info){
+    if (server_info->server_socket_desc == -1)
     {
-		// SOLICITACAO DE TRINCAS -- CLIENTE
-		if(buffer[0] == 0x10
-			|| buffer[0] == 0x11
-			|| buffer[0] == 0x12
-			|| buffer[0] == 0x13
-			|| buffer[0] == 0x14){
-
-			printf("** RECEBIDA SOLICITACAO DE TRINCAS **\n\n");
-
-			printf("<<--\n",buffer[0]);
-			printf("|=============|\n",buffer[0]);
-			printf("|| Cabecalho || \n");
-			printf("|=============|\n",buffer[0]);
-			printf("|     0x%hhx    |\n",buffer[0]);
-			printf("|=============|\n\n",buffer[0]);
-
-			int codon_number = 0;
-			if(buffer[0] == 0x10)
-				codon_number = 1;
-			else if(buffer[0] == 0x11)
-				codon_number = 2;
-			else if(buffer[0] == 0x12)
-				codon_number = 3;
-			else if(buffer[0] == 0x13)
-				codon_number = 4;
-			else if(buffer[0] == 0x14)
-				codon_number = 5;
-
-			srand( (unsigned)time( NULL ) );
-			//int random_number_codons = (rand() % 5)+1;
-			char codons[codon_number];
-			int i = 0;
-
-			printf("                 -->>\n",buffer[0]);	
-			printf("|====================|\n",buffer[0]);
-			printf("|| Trincas enviadas || \n");
-			printf("|====================|\n",buffer[0]);
-
-
-			for ( i = 0; i < sizeof(codons); ++i) {
-				codons[i] = (char)(rand() % 65);
-				printf("|        0x%x        |\n", codons[i] & 0xff);
-			}
-
-			printf("|====================|\n\n",buffer[0]);
-
-
-			//Send the message back to client
-			write(sock , codons , strlen(buffer));
-			//printf("|| IP: %s\n", inet_ntoa( ((struct sockaddr_in *) &socket_desc)->sin_addr));
-
-		}
-
-		// RESPOSTA DE UMA SOLICITACAO -- SERVIDOR
-		if(buffer[0] == 0x00
-			|| buffer[0] == 0x01
-			|| buffer[0] == 0x02
-			|| buffer[0] == 0x03
-			|| buffer[0] == 0x04){
-
-			printf("** RECEBIDA RESPOSTA COM TRINCAS **\n");
-		}
-    }
-
-    if(read_size == 0)
-    {
-        puts("-- CONEXAO DESFEITA --\n");
-        fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("xx recv falhou xx");
-    }
-
-    free(socket_desc);
-    close(sock);
-    pthread_exit(NULL); 
-}
-
-int handle_connection_threads(struct loc_info_struct *loc_info, struct sockaddr_in *server, struct sockaddr_in *client){
-	loc_info->socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (loc_info->socket_desc == -1)
-    {
-        printf("xx Nao foi possivel criar um socket xx");
+        printf("xx Não foi possível criar um socket xx");
     }
     printf("++ Socket criado ++\n");
 
-	//printf("%s", atoi(loc_info->port));
-
-	server->sin_family = AF_INET;
-    server->sin_addr.s_addr = INADDR_ANY;
-    server->sin_port = htons( loc_info->port );
-
-	if( bind(loc_info->socket_desc,(struct sockaddr *)server , sizeof(*server)) < 0)
+	if( bind(server_info->server_socket_desc, (struct sockaddr *)&server_info->loc_addr, sizeof(struct sockaddr)) < 0)
     {
         //print the error message
-        perror("zz Bind() falhou. Erro: ");
+        perror("Bind() falhou. Erro: ");
         return 1;
     }
     puts("++ Bind foi realizado ++");
+}
 
-	listen(loc_info->socket_desc , 3);
+int handle_connection_threads(struct server_info_struct *server_info){
+	listen(server_info->server_socket_desc , 3);
 
 	puts("... Esperando conexoes ...");
-    loc_info->c = sizeof(struct sockaddr_in);
+
+    int sockaddr_in_size = sizeof(struct sockaddr_in);
+	int client_sock_desc, *new_client_sock_desc;
+	struct sockaddr_in rem_addr;
 	
-	
-	while( loc_info->client_sock = accept(loc_info->socket_desc, (struct sockaddr *)&client, (socklen_t*)&loc_info->c)) {
-		puts("++ CONEXAO FEITA ++");
+	while( client_sock_desc = accept(server_info->server_socket_desc, (struct sockaddr *)&rem_addr, (socklen_t*)&sockaddr_in_size)) {
+		puts("\n++ CONEXAO FEITA ++");
 
 		pthread_t connection_thread;
-        loc_info->new_sock = malloc(sizeof *loc_info->new_sock);
-        *loc_info->new_sock = loc_info->client_sock;
+        new_client_sock_desc = malloc(sizeof *new_client_sock_desc);
+        *new_client_sock_desc = client_sock_desc;
 
-		if( pthread_create( &connection_thread , NULL ,  connection_handler , (void*) loc_info->new_sock) < 0)
+		if( pthread_create( &connection_thread , NULL ,  connection_handler , (void*) new_client_sock_desc) < 0)
         {
             perror("xx Nao foi possivel criar a thread xx");
             return 1;
         }
 	}
 
-	if (loc_info->client_sock < 0)
+	if (client_sock_desc < 0)
     {
         perror("Nao aceito");
         return 1;
     }
 }
+
+void *connection_handler(void *server_socket_desc){
+    int sock = *(int*)server_socket_desc;
+    int read_size;
+    char *message; 
+	aatp_msg recv_buffer;
+
+    while( (read_size = recv(sock , &recv_buffer , sizeof(recv_buffer) , 0)) > 0 ){
+		// SOLICITACAO DE TRINCAS -- CLIENTE
+		if(recv_buffer.method == 'S'){
+
+			printf("** RECEBIDA SOLICITACAO DE TRINCAS **\n\n");
+
+			printf("<<-- Solicitação recebida\n");
+			printf("|=============|\n");
+			printf("|| CABEÇALHO || \n");
+			printf("|-------------|\n");
+			printf("|     %c %i     |\n",recv_buffer.method, recv_buffer.size);
+			printf("|=============|\n\n");
+
+			aatp_msg response_packet = {0};
+			response_packet.method = 'R';
+			response_packet.size = recv_buffer.size;
+			memset(&response_packet.payload, 0, sizeof(response_packet));
+
+			srand( (unsigned)time( NULL ));
+
+			char random_aminoacids[20] = {'A','R','N','D','C','E','Q','G',
+										  'H','I','L','K','M','F','P','S','T','W',
+										  'Y','V'};
+			int i = 0;
+
+			printf("A.A. enviados     -->>\n");	
+			printf("|====================|\n");
+			printf("||     CABEÇALHO    || \n");
+			printf("|--------------------|\n");
+			printf("|         %c %i        |\n",response_packet.method, response_packet.size);
+
+
+			printf("|====================|\n");
+			printf("||       DADOS      || \n");
+			printf("|--------------------|\n");
+
+
+			for ( i = 0; i < recv_buffer.size; ++i) {
+				int random_index = (rand() % 20);
+				response_packet.payload[i] = random_aminoacids[random_index];
+				printf("|          %c         |\n", response_packet.payload[i]);
+			}
+
+			printf("|====================|\n\n");
+
+
+			//Send the message back to client
+			write(sock , &response_packet , sizeof(response_packet));
+			//printf("|| IP: %s\n", inet_ntoa( ((struct sockaddr_in *) &socket_desc)->sin_addr));
+			
+		}
+
+		// RESPOSTA DE UMA SOLICITACAO -- SERVIDOR
+		if(recv_buffer.method == 'R'){
+			printf("** RECEBIDA RESPOSTA COM TRINCAS **\n");
+		}
+    }
+
+    if(read_size == 0){
+        puts("-- CONEXAO DESFEITA --\n");
+        fflush(stdout);
+    }
+    else if(read_size == -1){
+        perror("xx recv falhou xx");
+    }
+
+    free(server_socket_desc);
+    close(sock);
+    pthread_exit(NULL); 
+}
+
+void send_aminoacid_solicitation(struct server_info_struct *server_info){
+	if (connect(server_info->server_socket_desc, (struct sockaddr *) &server_info->loc_addr, sizeof(server_info->loc_addr)) < 0) {
+		perror("Conectando stream socket");
+		exit(1);
+	}
+
+	char cat[] = { 0x13 };
+
+	send(server_info->server_socket_desc, cat, sizeof(cat), 0);
+
+	/* fechamento do socket remota */ 	
+	close(server_info->server_socket_desc);
+}
+
+void *solicitation_sender_handler(){
+}
+
 
 int main(int argc, char *argv[]) {
 	//Verifica se foi colocado argumento da porta
@@ -222,15 +238,22 @@ int main(int argc, char *argv[]) {
 	
 	// == Controla as threads do cliente
 
-	struct loc_info_struct loc_info = {socket(AF_INET , SOCK_STREAM , 0),
-										0,
-										sizeof(struct sockaddr_in),
-										0,
-										atoi(argv[1])};
-	
-	struct sockaddr_in server, client;
+	struct server_info_struct server_info;
 
-	handle_connection_threads(&loc_info, &server, &client);
+	initialize_server_info(atoi(argv[1]),&server_info);
+	initialize_server_socket(&server_info);
+
+	//pthread_t solicitation_sender_thread;
+	/*if( pthread_create( &solicitation_sender_thread , NULL ,  solicitation_sender_handler , (void*) loc_info->new_sock) < 0)
+        {
+            perror("xx Nao foi possivel criar a thread de envio de solicitacoes xx");
+            return 1;
+        }
+	}*/
+	send_aminoacid_solicitation(&server_info);
+	handle_connection_threads(&server_info);
+
+	printf("saiu");
 
     return 0;
 
